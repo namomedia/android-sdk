2222063064fa97e408ec7ae50c89a8c9004d1b94
Joe Blubaugh <joe@namomedia.com>
2014-01-31 11:23:42 -0800

    Version 2.1.0

--- namo_lib/src/com/namomedia/android/UnusedStub.java
+++ namo_lib/src/com/namomedia/android/UnusedStub.java
@@ -1,6 +1,8 @@
 package com.namomedia.android;
 
-// Stub java file to make inclusion into some IDE's work.
+// Stub java file to make inclusion into some IDE's (Eclipse) work when
+// importing the final library project. The compiled Namo source
+// code is packaged as a jar file in your libs directory.
 public final class UnusedStub {
     private UnusedStub() { }
 }
